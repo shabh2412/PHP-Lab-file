abcb9709ed7341606ba8eddf5477c377e950b069
Rishabh <rishabhpanesar@gmail.com>
2020-04-24 13:25:37 +0530

    Added script to send mail using php mailer

--- EXP17/index.php
+++ EXP17/index.php
@@ -7,18 +7,18 @@
     $mail -> Host = 'smtp.gmail.com';
     $mail -> Port = 465;
     $mail->isHTML();
-    $mail->Username = 'automated.rishabh.panesar@gmail.com';
-    $mail->Password = 'R#$%HHJK';
-    $mail->setFrom('automated.rishabh.panesar@gmail.com');
-    $mail->Subject = 'Automated Mail by Rishabh Panesar';
+    $mail->Username = 'Your Email';
+    $mail->Password = 'Your Password';
+    $mail->setFrom('Your Email');
+    $mail->Subject = 'Subject of the mail';
     $mail->Body = 'Hi there! This is an automated test email which has been sent using PHP';
-    $saga = 'sagarikasahoo16@gmail.com';
-    $rish = 'rishabh.panesar2609@gmail.com';
-    $mail->addAddress($rish);
-    $mail->addCC($saga);
-    $self = 'automated.rishabh.panesar@gmail.com';
-    $mail->addReplyTo($self,'Rishabh Panesar');
-    if($mail->validateAddress($rish,FILTER_VALIDATE_EMAIL)){
+    $recipient1 = 'reciever\'s email address';
+    $recipient2 = '2nd recievers email address';
+    $mail->addAddress($recipient2);
+    $mail->addCC($recipient1);
+    $self = 'your email';
+    $mail->addReplyTo($self,'your name');
+    if($mail->validateAddress($recipient2,FILTER_VALIDATE_EMAIL)){
         echo "Validated";
         if (!$mail->send()) { echo "Mailer Error: " . $mail->ErrorInfo; } else { 
             echo "Message sent!";
